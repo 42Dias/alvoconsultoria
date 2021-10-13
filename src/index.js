736ceb55dd64388f1c9cf53e952e60ce71c28208
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-13 17:51:23 -0300

    add configuring

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
->>>>>>> 021fde2a1e1f4f37e55b53b35010ee893ef9feb1
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
   document.getElementById('root'),
 )
-=======
-  document.getElementById('root')
-);
->>>>>>> 021fde2a1e1f4f37e55b53b35010ee893ef9feb1
+
