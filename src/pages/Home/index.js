652e84c502bb04cd684316898d685544f448cf3f
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2021-10-13 17:21:43 -0300

    componente header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="container">
       <h2>Pagina home</h2>
-      <Link to="/entrar">Entrar</Link>
-      <Link to="/cadastrar">Cadastrar</Link>
+      <Link to="/entrar">Entrar</Link><br />
+      <Link to="/cadastrar">Cadastrar</Link><br />
       <Link to="/esqueceu-a-senha">Esqueceu a senha</Link>
     </div>
   );
