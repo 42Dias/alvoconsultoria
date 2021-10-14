29256e452d6359b6948bc5e21bb7a75ee92098ba
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2021-10-14 09:59:45 -0300

    teste

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,30 +2,32 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <body className="bg-primary">
-      <div class="container">
-        <section>
-          <article class="d-flex">
-            <div class="content  col-6">
-              <h2 class="font-weight-500">
-                A plataforma número um para consultoria de TCC.
-              </h2>
-              <p>
-                Com a Alvo, você tem a tranquilidade e segurança para conquistar
-                a tão sonhada aprovação!
-              </p>
-              <div class="d-flex align-items-center mt-2">
-                <a href="" class="mr-2 btn-patterns">
-                  Criar minha conta
-                </a>
-                <a href="" class="link-conta">
-                  Ja tenho conta!
-                </a>
-              </div>
-            </div>
-          </article>
-        </section>
-      </div>
-    </body>
+    <div className="image-banner">
+      <div className="bg-primary">
+          <div class="container">
+            <section>
+              <article class="d-flex">
+                <div class="content  col-6">
+                  <h2 class="font-weight-500">
+                    A plataforma número um para consultoria de TCC.
+                  </h2>
+                  <p>
+                    Com a Alvo, você tem a tranquilidade e segurança para conquistar
+                    a tão sonhada aprovação!
+                  </p>
+                  <div class="d-flex align-items-center mt-2">
+                    <a href="" class="mr-2 btn-patterns">
+                      Criar minha conta
+                    </a>
+                    <a href="" class="link-conta">
+                      Ja tenho conta!
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </section>
+          </div>
+        </div>
+    </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ const Routes = () => {
         <Route path='*' component={Home} />
       </Switch>
     </BrowserRouter>
+
   )
 }
 
