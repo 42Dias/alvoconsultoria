9b375f4af863de1f715d182f871ed8a48929b724
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-14 17:59:02 -0300

    add styled components

---
+++ src/pages/SignIn/styles/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ContainerLogin = styled.div`
+  height: 85vh !important;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  background: linear-gradient(
+    270deg,
+    #c4c4c4 9.21%,
+    rgba(196, 196, 196, 0) 85.55%
+  );
+`
+
+export const Background = styled.div``
