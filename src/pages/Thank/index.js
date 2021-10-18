0d89d7b44cf6586075444a550875ad0fc0f44a2e
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-18 09:19:27 -0300

    styled components of the thank screen

--- src/pages/Thank/index.js
+++ src/pages/Thank/index.js
@@ -1,26 +1,22 @@
-import './styles/thanks.css'
+import * as S from './styles/styles'
 
 export default function Thank() {
   return (
-    <>
-      <div className='containerLogin'>
-        <div className='bg-thanks'>
-          <div className='container'>
-            <section className='content-login-form'>
-              <div className=''>
-                <h2 className='text-white'>
-                  Obrigada por querer se tornar <br /> um prestador de serviços
-                  Alvo!
-                </h2>
-                <p className='text-white'>
-                  Fique atento ao seu e-mail que retornaremos em, no máximo, 15
-                  dias com feedback
-                </p>
-              </div>
-            </section>
-          </div>
-        </div>
-      </div>
-    </>
+    <S.ThankContainer>
+      <S.Background>
+        <S.Container>
+          <S.ContentLoginForm>
+            <h2>
+              Obrigada por querer se tornar <br /> um prestador de serviços
+              Alvo!
+            </h2>
+            <p>
+              Fique atento ao seu e-mail que retornaremos em, no máximo, 15 dias
+              com feedback
+            </p>
+          </S.ContentLoginForm>
+        </S.Container>
+      </S.Background>
+    </S.ThankContainer>
   )
 }
